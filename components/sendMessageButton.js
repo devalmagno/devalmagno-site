a92a281732bb4e61d409db0988dc54cf60e0596e
devalmagno <devalmagno@gmail.com>
2021-02-13 22:42:11 -0300

    Desktop and mobile interface completed, but in the future will have updates

--- components/sendMessageButton.js
+++ components/sendMessageButton.js
@@ -3,7 +3,7 @@ import styles from './sendMessageButton.module.css';
 export default function SendMessageButton() {
     return (
         <button className={styles.sendMessageButton}>
-            <a>
+            <a target="_blank" href="https://api.whatsapp.com/send?phone=5538998925788&text=Ol%C3%A1%2C%20L%C3%BAcio.%20Estou%20interessado%20nos%20seus%20servi%C3%A7os%20e%20desejo%20te%20contratar.">
                 Entrar em contato
             </a>
         </button>
--- pages/index.js
+++ pages/index.js
@@ -10,18 +10,19 @@ function HomePage() {
                     Lúcio Magno
                 </span>
 
-                <a className={homecss.githubDesktop} target="_blank" href="https://github.com/devalmagno">
-                    Veja meu GitHub
-                </a>
-
                 <button className={homecss.githubMobile}>
                     <a target="_blank" href="https://github.com/devalmagno">
                         <img src="/images/icons/github.svg" />
-                        Veja meu GitHub
-                    </a>
+                            Veja meu GitHub
+                        </a>
                 </button>
 
-                <SendMessageButton />
+                <div className={homecss.buttonsDesktop}>
+                    <a className={homecss.githubDesktop} target="_blank" href="https://github.com/devalmagno">
+                        Acesse meu GitHub
+                    </a>
+                    <SendMessageButton />
+                </div>
             </header>
 
             <main>
@@ -35,34 +36,34 @@ function HomePage() {
 
                 <div>
                     <div className={homecss.stackDesktop}>
-                        <a>
+                        <a target="_blank" href="https://pt-br.reactjs.org/">
                             <img src="/images/icons/reactjs-large.svg" />
                         </a>
-                        <a>
+                        <a target="_blank" href="https://nextjs.org/">
                             <img src="/images/icons/nextjs-large.svg" />
                         </a>
-                        <a>
+                        <a target="_blank" href="https://nodejs.org/en/">
                             <img src="/images/icons/nodejs-large.svg" />
                         </a>
-                        <a>
+                        <a target="_blank" href="https://expressjs.com/pt-br/">
                             <img src="/images/icons/express-large.svg" />
                         </a>
-                        <a>
+                        <a target="_blank" href="https://www.typescriptlang.org/">
                             <img src="/images/icons/typescript-large.svg" />
                         </a>
                     </div>
 
                     <div className={homecss.stackMobile}>
-                        <a>
+                        <a target="_blank" href="https://pt-br.reactjs.org/">
                             <img src="/images/icons/reactjs-small.svg" />
                         </a>
-                        <a>
+                        <a target="_blank" href="https://nextjs.org/">
                             <img src="/images/icons/nextjs-small.svg" />
                         </a>
-                        <a>
+                        <a target="_blank" href="https://nodejs.org/en/">
                             <img src="/images/icons/nodejs-small.svg" />
                         </a>
-                        <a>
+                        <a target="_blank" href="https://www.typescriptlang.org/">
                             <img src="/images/icons/typescript-small.svg" />
                         </a>
                     </div>
